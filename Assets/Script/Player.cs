457a9feca3aef4e82f547780fb39974a5748086e
01AntonioFagundes <luizantoniocardoso2008@gmail.com>
2026-09-22 09:12:14 -0300

    Limitador de pulo
    fizemos uma programação que reconhece quando o jogador está no chão, ou não está no chão, permitindo pular apenas quando está no chão

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,7 +4,7 @@ public class Player : MonoBehaviour
 {
     public float speed = 5f;
     private Rigidbody2D rb;
-
+    private bool isGrounded = false;
     void Start()
     {
     rb = GetComponent<Rigidbody2D>();    
@@ -16,10 +16,26 @@ public class Player : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal"); //reconhece as teclas A e D como moviemnto horizontal
         rb.linearVelocity = new Vector2(moveHorizontal * speed, rb.linearVelocity.y); //adiciona a velocidade de movimento
 
-        if (Input.GetKeyDown(KeyCode.Space)) //reconhece a barra de espaço 
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded) //reconhece a barra de espaço 
         {
             rb.AddForce(new Vector2(0f, 5f), ForceMode2D.Impulse); // adiciona uma força no pulo
+        }
 
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isGrounded = true; //reconhece quando o jogador está no chao
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("ground"))
+        {
+            isGrounded = false; //reconhece quando o jogador não está no chao
         }
     }
 }
